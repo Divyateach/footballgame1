aebb26e62c7aa08a5e012b2e76f33bdc7197900f
Divyateach <74509757+Divyateach@users.noreply.github.com>
2021-02-13 22:50:53 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# footballgame1
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint=Matter.Constraint;
+var player,football,goalpost,ground,launcher;
+function preload()
+{
+	player=loadImage("images/player.png")
+}
+
+function setup() {
+	createCanvas(1300, 600);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+football=new Football(100,570,30,30)
+goalpost=new Goalpost(1100,350,300,400)
+ground=new Ground(650,580,1300,20)
+launcher=new Launcher(football.body,{x:180,y:300})
+	//Engine.run(engine);
+  
+}
+
+
+function draw() {
+  
+  background("white");
+  Engine.update(engine)
+  image(player,50,260,200,300);
+football.display();
+goalpost.display();
+ground.display();
+ launcher.display();
+}
+
+
+
